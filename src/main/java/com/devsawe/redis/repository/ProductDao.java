e1d2de0302e6021140f6dbcb1ddcb3aa4a2f7b8f
s-sawe <sawesharonn@gmail.com>
2022-08-15 13:13:52 +0300

    Initial commit

---
+++ src/main/java/com/devsawe/redis/repository/ProductDao.java
@@ -0,0 +1,31 @@
+package com.devsawe.redis.repository;
+
+import com.devsawe.redis.entity.Product;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDao {
+
+    public static final String HASH_KEY = "Product";
+    private RedisTemplate template;
+    public Product save(Product product){
+        template.opsForHash().put(HASH_KEY,product.getId(),product);
+        return product;
+    }
+
+    public List<Product> findAll(){
+        return template.opsForHash().values(HASH_KEY);
+    }
+
+    public Product findProductById(int id){
+        return (Product) template.opsForHash().get(HASH_KEY,id);
+    }
+
+    public String deleteProduct(int id){
+        template.opsForHash().delete(HASH_KEY,id);
+        return "product removed";
+    }
+}
